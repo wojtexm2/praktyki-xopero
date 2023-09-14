865f31d2f21b618af16f53e696b9034f09e9bf07
wojtexm2 <wblajda@gmail.com>
2023-09-14 12:48:35 +0200

    Program 6

---
+++ programy/rozdział2/program6 (coś błyskotliwego)/program6 (coś błyskotliwego)/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace program6__coś_błyskotliwego_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            while (Visible)
+            {
+                for (int c = 0; c <= 255; c++)
+                {
+                    Application.DoEvents();
+                    this.BackColor = Color.FromArgb(c, 255 - c, c);
+                    System.Threading.Thread.Sleep(3);
+                }
+                for (int c = 255; c >= 0; c--)
+                {
+                    Application.DoEvents();
+                    this.BackColor = Color.FromArgb(c, 255 - c, c);
+                    System.Threading.Thread.Sleep(3);
+                }
+            }
+            
+        }
+    }
+}
