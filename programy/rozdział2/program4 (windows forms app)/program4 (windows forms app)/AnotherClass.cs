561d53f8fd7e4634b3cf65cd9c1b9b2b7769a1b2
wojtexm2 <wblajda@gmail.com>
2023-09-14 11:45:57 +0200

    dodatkowe zad do programu4

---
+++ programy/rozdział2/program4 (windows forms app)/program4 (windows forms app)/AnotherClass.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program4__windows_forms_app_
+{
+    internal class AnotherClass
+    {
+        public static void main()
+        {
+            MessageBox.Show("Bum!");
+        }
+    }
+}
