2098fd92628e5b9a0c681ec3b0293043b2f14fe9
wojtexm2 <wblajda@gmail.com>
2023-09-14 11:25:18 +0200

    zakończony Program 4

---
+++ programy/rozdział2/program4 (windows forms app)/program4 (windows forms app)/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace program4__windows_forms_app_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (enableCheckbox.Checked == true)
+            {
+                if (labelToChange.Text == "Z prawej")
+                {
+                    labelToChange.Text = "Z lewej";
+                    labelToChange.TextAlign = ContentAlignment.MiddleLeft;
+                }
+                else
+                {
+                    labelToChange.Text = "Z prawej";
+                    labelToChange.TextAlign = ContentAlignment.MiddleRight;
+                }
+            }
+            else
+            {
+                labelToChange.Text = "Możliwość zmiany tekstu została wyłączona";
+                labelToChange.TextAlign = ContentAlignment.MiddleCenter;
+            }
+        }
+    }
+}
---
+++ programy/rozdział2/program4 (windows forms app)/program4 (windows forms app)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace program4__windows_forms_app_
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
