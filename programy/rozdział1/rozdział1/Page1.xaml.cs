9163e277c5608a160f0df122de7c65ff1e92d75c
wojtexm2 <wblajda@gmail.com>
2023-09-14 08:51:12 +0200

    Opuszczenie projektu
    Software ukazany w książce jest przestarzały i wykonanie projektu jest prawie niemożliwe

--- programy/rozdział1/rozdział1/Page1.xaml.cs
+++ programy/rozdział1/rozdział1/Page1.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+//using Windows.UI.Xaml.Media.Animation;
+using System.Windows.Media.Animation;
+
 namespace rozdział1
 {
     /// <summary>
@@ -20,6 +23,7 @@ namespace rozdział1
     /// </summary>
     public partial class Page1 : Page
     {
+        Random random = new Random();
         public Page1()
         {
             InitializeComponent();
@@ -32,7 +36,27 @@ namespace rozdział1
 
         private void startButton_Click(object sender, RoutedEventArgs e)
         {
+            AddEnemy();
+        }
 
+        private void AddEnemy()
+        {
+            ContentControl enemy = new ContentControl();
+            enemy.Template = Resources["enemyTemplate"] as ControlTemplate;
+            AnimateEnemy(enemy, 0, playArea.ActualWidth - 100, "(Canvas.Left)");
+            AnimateEnemy(enemy, random.Next((int)playArea.ActualHeight - 100), random.Next((int)playArea.ActualHeight-100), "(Canvas.Top)");
+            playArea.Children.Add(enemy);
+        }
+
+        private void AnimateEnemy(ContentControl enemy, double from, double to, string propertyToAnimate)
+        {
+            Storyboard storyboard = new Storyboard() { AutoReverse = true, RepeatBehavior = RepeatBehavior.Forever };
+            DoubleAnimation animation = new DoubleAnimation()
+            {
+                From = from,
+                To = to,
+                Duration = new Duration(TimeSpan.FromSeconds(random.Next(4, 6)))
+            };
         }
     }
 }
