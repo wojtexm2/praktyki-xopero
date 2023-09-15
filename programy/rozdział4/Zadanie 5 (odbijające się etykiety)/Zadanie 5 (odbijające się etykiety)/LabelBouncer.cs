c9783c3b34d4c8e817ba6499b9790d7330a0b614
wojtexm2 <wblajda@gmail.com>
2023-09-15 13:01:27 +0200

    R4P5
    Rozdział 4 program 5

---
+++ programy/rozdział4/Zadanie 5 (odbijające się etykiety)/Zadanie 5 (odbijające się etykiety)/LabelBouncer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_5__odbijające_się_etykiety_
+{
+    internal class LabelBouncer
+    {
+        public Label MyLabel;
+        public bool GoingForward = true;
+
+        public void Move()
+        {
+            if (MyLabel != null)
+            {
+                if (GoingForward == true)
+                {
+                    MyLabel.Left += 5;
+                    if (MyLabel.Left >= MyLabel.Parent.Width - MyLabel.Width)
+                    {
+                        GoingForward = false;
+                    }
+                }
+                else
+                {
+                    MyLabel.Left -= 5;
+                    if (MyLabel.Left <= 0)
+                    {
+                        GoingForward = true;
+                    }
+                }
+            }
+        }
+    }
+}
