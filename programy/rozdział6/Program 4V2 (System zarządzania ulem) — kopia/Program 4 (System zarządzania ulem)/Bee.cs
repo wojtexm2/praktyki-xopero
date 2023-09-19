d06f9ed29e1e1493c13ac2aaadb691a9549b95f3
wojtexm2 <wblajda@gmail.com>
2023-09-19 13:06:58 +0200

    R6P4V2
    Rozdział 6 program 4V2

---
+++ programy/rozdział6/Program 4V2 (System zarządzania ulem) — kopia/Program 4 (System zarządzania ulem)/Bee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_4__System_zarządzania_ulem_
+{
+    internal class Bee
+    {
+        public const double HoneyUnitsConsumedPerMg = .25;
+        public double WeightMg { get; private set; }
+        public Bee(double weightMg)
+        {
+            WeightMg = weightMg;
+        } 
+        virtual public double HoneyConsumptionRate()
+        {
+            return WeightMg * HoneyUnitsConsumedPerMg;
+        }
+    }
+}
--- programy/rozdział6/Program 4V2 (System zarządzania ulem) — kopia/Program 4 (System zarządzania ulem)/Queen.cs
+++ programy/rozdział6/Program 4V2 (System zarządzania ulem) — kopia/Program 4 (System zarządzania ulem)/Queen.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace Program_4__System_zarządzania_ulem_
 {
-    internal class Queen
+    internal class Queen : Bee
     {
-        public Queen(Worker[] workers)
+        public Queen(Worker[] workers, double weightMg) : base(weightMg)
         {
             this.workers = workers;
         }
@@ -25,10 +25,12 @@ namespace Program_4__System_zarządzania_ulem_
         }
         public string WorkTheNextShift()
         {
+            double honeyConsumed = HoneyConsumptionRate();
             shiftNumber++;
             string report = "Raport zmiany numer " + shiftNumber + "\r\n";
             for (int i = 0; i < workers.Length; i++)
             {
+                honeyConsumed += workers[i].HoneyConsumptionRate();
                 if (workers[i].DidYouFinish()) report += "Robotnica numer " + (i + 1) + " zakończyła swoje zadanie\r\n";
                 if (String.IsNullOrEmpty(workers[i].CurrentJob)) report += "Robotnica numer " + (i + 1) + " nie pracuje\r\n";
                 else if (workers[i].ShiftsLeft > 0)
@@ -38,6 +40,7 @@ namespace Program_4__System_zarządzania_ulem_
                 }
                 else report += "Robotnica numer " + (i + 1) + " zakończy '" + workers[i].CurrentJob + "' po tej zmianie\r\n";
             }
+            report += "Całkowite spożycie miodu: " + honeyConsumed + " jednostek\r\n";
             return report;
         }
     }
