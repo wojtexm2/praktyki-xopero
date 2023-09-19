6d64cc33ef1e2ac7b4f1db7290eaf5be4119423d
wojtexm2 <wblajda@gmail.com>
2023-09-19 12:05:43 +0200

    Funkcjonalne klasy

---
+++ programy/rozdział6/Program 4 (System zarządzania ulem)/Program 4 (System zarządzania ulem)/Form1.cs
@@ -0,0 +1,10 @@
+namespace Program_4__System_zarządzania_ulem_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
