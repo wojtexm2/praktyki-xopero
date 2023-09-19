c4eefc5f3af9957a67d91555871ef2092647b801
wojtexm2 <wblajda@gmail.com>
2023-09-19 14:02:53 +0200

    R7P1
    Rozdział 7 Program 1

---
+++ programy/Rozdział7/Program 1 (Test interface)/Program 1 (Test interface)/IClown.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_1__Test_interface_
+{
+    internal interface IClown
+    {
+        void Honk();
+        string FunnyThingIHave { get; }
+    }
+}
