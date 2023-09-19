393e47b8abad1a0970a20dc8459c99fab50a2a44
wojtexm2 <wblajda@gmail.com>
2023-09-19 14:42:46 +0200

    R7P1V2
    Rozdział 7 Program 1 2 wersja ćwiczenia

---
+++ programy/Rozdział7/Program 1V2 (Dziedziczenie interfaceów)/Program 1 (Test interface)/TallGuy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_1__Test_interface_
+{
+    internal class TallGuy : IClown
+    {
+        public string Name;
+        public int Height;
+        public void TalkAboutYourself()
+        {
+            Console.WriteLine("Nazywam się " + Name + " i mam " + Height + " centymetrów wzrostu.");
+        }
+        public string FunnyThingIHave
+        {
+            get { return "duże buty"; }
+        }
+        public void Honk()
+        {
+            Console.WriteLine("Tut tuut!");
+        }
+    }
+}
