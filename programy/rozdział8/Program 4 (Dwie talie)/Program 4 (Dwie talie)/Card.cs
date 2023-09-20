4cf3584f53f1b736a9b33619859d3d0989c9593d
wojtexm2 <wblajda@gmail.com>
2023-09-20 13:58:52 +0200

    Część programu 4
    Formularz, klasa Deck, Card i interfejsy

---
+++ programy/rozdział8/Program 4 (Dwie talie)/Program 4 (Dwie talie)/Card.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_4__Dwie_talie_
+{
+    internal class Card
+    {
+        public Values Value;
+        public Suits Suit;
+        public Card(Suits suit, Values value)
+        {
+            Value = value;
+            Suit = suit;
+        }
+    }
+}
---
+++ programy/rozdział8/Program 4 (Dwie talie)/Program 4 (Dwie talie)/Deck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_4__Dwie_talie_
+{
+    internal class Deck
+    {
+        private List<Card> cards;
+        private Random random = new Random();
+
+        public Deck()
+        {
+            cards = new List<Card>();
+            for (int suit = 0; suit <= 3; suit++)
+            for (int value = 1; value <= 13;  value++)
+            cards.Add(new Card((Suits)suit, (Values)value));
+        }
+    }
+}
