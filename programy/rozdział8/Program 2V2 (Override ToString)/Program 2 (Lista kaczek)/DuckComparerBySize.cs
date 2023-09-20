9568cc06e5534e7aa9ce79c5f358b5fcb8f338b1
wojtexm2 <wblajda@gmail.com>
2023-09-20 12:51:20 +0200

    R8P2V2
    Rozdział 8 Program 2 Wersja 2

---
+++ programy/rozdział8/Program 2V2 (Override ToString)/Program 2 (Lista kaczek)/DuckComparerBySize.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_2__Lista_kaczek_
+{
+    internal class DuckComparerBySize : IComparer<Duck>
+    {
+        public int Compare(Duck x, Duck y)
+        {
+            if (x.Size < y.Size) return -1;
+            if (x.Size > y.Size) return 1;
+            else return 0;
+        }
+    }
+}
---
+++ programy/rozdział8/Program 2V2 (Override ToString)/Program 2 (Lista kaczek)/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Program_2__Lista_kaczek_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Duck> ducks = new List<Duck>()
+            {
+                new Duck(){ Kind = KindOfDuck.Mallard, Size = 17 },
+                new Duck(){ Kind = KindOfDuck.Muscovy, Size = 18 },
+                new Duck(){ Kind = KindOfDuck.Decoy, Size = 14 },
+                new Duck(){ Kind = KindOfDuck.Muscovy, Size = 11 },
+                new Duck(){ Kind = KindOfDuck.Mallard, Size = 14 },
+                new Duck(){ Kind = KindOfDuck.Decoy, Size = 13 },
+            };
+            DuckComparerBySize sizeComparer = new DuckComparerBySize();
+            DuckComparerByKind kindComparer = new DuckComparerByKind();
+            DuckComparer comparer = new DuckComparer();
+
+            PrintDucks(ducks);
+            ducks.Sort(sizeComparer);
+            PrintDucks(ducks);
+            ducks.Sort(kindComparer);
+            PrintDucks(ducks);
+            comparer.SortBy = SortCriteria.KindThenSize;
+            ducks.Sort(comparer);
+            PrintDucks(ducks);
+            Console.ReadKey();
+        }
+        public static void PrintDucks(List<Duck> ducks)
+        {
+            foreach (Duck duck in ducks)
+            {
+                Console.WriteLine(duck);
+            }
+            Console.WriteLine("Koniec kaczek!");
+        }
+    }
+}
