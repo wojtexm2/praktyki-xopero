641f7b07f506407af9a0c3b0574f1facf5acaa3b
wojtexm2 <wblajda@gmail.com>
2023-09-20 12:29:51 +0200

    R8P3
    Rozdział 8 Program 3

---
+++ programy/rozdział8/Program 3 (losowanie i sortowanie kart)/Program 3 (losowanie i sortowanie kart)/Suits.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_3__losowanie_i_sortowanie_kart_
+{
+    internal enum Suits
+    {
+        Hearts,
+        Spades,
+        Diamonds,
+        Clubs
+    }
+}
