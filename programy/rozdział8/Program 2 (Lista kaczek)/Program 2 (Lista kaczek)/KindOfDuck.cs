68bb35d1abadbaa9d3f0a57528712b8432ee0de7
wojtexm2 <wblajda@gmail.com>
2023-09-20 11:59:14 +0200

    R8P2
    Rozdział 8 Program 2

---
+++ programy/rozdział8/Program 2 (Lista kaczek)/Program 2 (Lista kaczek)/KindOfDuck.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_2__Lista_kaczek_
+{
+    internal enum KindOfDuck
+    {
+        Mallard,
+        Muscovy,
+        Decoy
+    }
+}
