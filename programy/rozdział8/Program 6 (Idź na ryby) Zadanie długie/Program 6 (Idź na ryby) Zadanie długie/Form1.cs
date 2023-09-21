d2510bf3e1f936c09f7fdfdcb659bdc49cd01c0b
wojtexm2 <wblajda@gmail.com>
2023-09-21 08:49:01 +0200

    Gotowe UI
    R8P6 (long)

---
+++ programy/rozdział8/Program 6 (Idź na ryby) Zadanie długie/Program 6 (Idź na ryby) Zadanie długie/Form1.cs
@@ -0,0 +1,10 @@
+namespace Program_6__Idź_na_ryby__Zadanie_długie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
