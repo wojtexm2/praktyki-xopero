cff6385433645881192bb0dd7feca9827fcdeb57
wojtexm2 <wblajda@gmail.com>
2023-09-22 14:29:16 +0200

    Punkt odzyskiwania zadania
    Tyle już siedzę że jakby program się wysypał to chociaż będę mógł przywrócić zmiany

---
+++ programy/rozdział9/Program 2 (Zarządzanie wymówkami)/Program 2 (Zarządzanie wymówkami)/Form1.cs
@@ -0,0 +1,65 @@
+using System.Runtime.CompilerServices;
+
+namespace Program_2__Zarządzanie_wymówkami_
+{
+    public partial class Form1 : Form
+    {
+        private Excuse currentExcuse = new Excuse();
+        private string selectedFolder = "";
+        private bool formChanged = false;
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void folder_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = selectedFolder;
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                selectedFolder = folderBrowserDialog1.SelectedPath;
+                save.Enabled = true;
+                open.Enabled = true;
+                randomExcuse.Enabled = true;
+            }
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(description.Text) || String.IsNullOrEmpty(results.Text))
+            {
+                MessageBox.Show("Okreśł wymówkę i rezultat", "Nie można zapisać pliku", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            saveFileDialog1.InitialDirectory = selectedFolder;
+            saveFileDialog1.Filter = "Pliki tekstowe (*.txt)|*.txt|Wszystkie pliki (*.*)|*.*";
+            saveFileDialog1.FileName = description.Text + ".txt";
+            DialogResult result = saveFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                currentExcuse.Save(saveFileDialog1.FileName);
+                UpdateForm(false);
+                MessageBox.Show("Wymówka zapisana");
+            }
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            if (CheckChanged())
+            {
+                openFileDialog1.InitialDirectory = selectedFolder;
+                openFileDialog1.Filter = "Pliki tekstowe (*.txt)|*.txt|Wszystkie pliki (*.*)|*.*|日本のファイル　（*.日本語）|*.日本語";
+                openFileDialog1.FileName = description.Text + ".txt";
+                DialogResult result = openFileDialog1.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    currentExcuse = new Excuse(openFileDialog1.FileName);
+                    UpdateForm(false);
+                }
+            }
+        }
+    }
+
+}
