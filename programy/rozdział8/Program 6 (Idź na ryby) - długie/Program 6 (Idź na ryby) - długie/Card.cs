e841a10985d18da697601b3ff28a0ffcf4400700
wojtexm2 <wblajda@gmail.com>
2023-09-22 08:35:31 +0200

    Dodano klasy
    Ulepszona klasa Deck,
    Card,
    Oraz Enum:
    Values
    Suits

---
+++ programy/rozdział8/Program 6 (Idź na ryby) - długie/Program 6 (Idź na ryby) - długie/Card.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_6__Idź_na_ryby____długie
+{
+    internal class Card : IComparable<Card>
+    {
+        public Values Value;
+        public Suits Suit;
+
+        public string Name
+        {
+            get
+            {
+                return Value.ToString() + " of " + Suit.ToString();
+            }
+        }
+        public Card(Suits suit, Values value)
+        {
+            this.Value = value;
+            this.Suit = suit;
+        }
+        public int CompareTo(Card other)
+        {
+            if (Suit > other.Suit) return -1;
+            if (Suit < other.Suit) return 1;
+            if (Value > other.Value) return 1;
+            if (Value < other.Value) return -1;
+            return 0;
+        }
+    }
+}
