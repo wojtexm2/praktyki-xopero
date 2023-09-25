16110c813b694216ad55f6bff657b1aae73ee600
wojtexm2 <wblajda@gmail.com>
2023-09-25 09:28:46 +0200

    R9P4
    Rozdział 9 Program 4

---
+++ programy/rozdział9/Program4 (Serializacja kart)/Program 4 (Serializacja kart)/CardComparer_byValue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_4__Serializacja_kart_
+{
+    [Serializable]
+    internal class CardComparer_byValue: IComparer<Card>
+    {
+        public int Compare(Card x, Card y)
+        {
+            if (x.Value > y.Value) return 1;
+            if (y.Value < x.Value) return -1;
+            if (x.Suit > y.Suit) return -1;
+            if (x.Suit < y.Suit) return 1;
+            return 0;
+        }
+    }
+}
---
+++ programy/rozdział9/Program4 (Serializacja kart)/Program 4 (Serializacja kart)/Suits.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_4__Serializacja_kart_
+{
+    [Serializable]
+    internal enum Suits
+    {
+        Hearts,
+        Spades,
+        Diamonds,
+        Clubs
+    }
+}
