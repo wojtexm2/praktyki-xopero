3854dbd27d347c44ccb8eb662be7f73aea26f59f
wojtexm2 <wblajda@gmail.com>
2023-09-25 10:51:59 +0200

    R9P8
    Rozdział 9 Program 8

---
+++ programy/rozdział9/Program 8 (Zarządzanie wymówkami - serjalizacja)/Program 2 (Zarządzanie wymówkami)/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Program_2__Zarządzanie_wymówkami_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            description = new TextBox();
+            results = new TextBox();
+            fileDate = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            folder = new Button();
+            save = new Button();
+            open = new Button();
+            randomExcuse = new Button();
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            saveFileDialog1 = new SaveFileDialog();
+            openFileDialog1 = new OpenFileDialog();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(63, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Wymówka";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 38);
+            label2.Name = "label2";
+            label2.Size = new Size(43, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Wyniki";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 67);
+            label3.Name = "label3";
+            label3.Size = new Size(83, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Ostatnio użyte";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 96);
+            label4.Name = "label4";
+            label4.Size = new Size(60, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Data pliku";
+            // 
+            // description
+            // 
+            description.Location = new Point(101, 6);
+            description.Name = "description";
+            description.Size = new Size(421, 23);
+            description.TabIndex = 4;
+            description.TextChanged += description_TextChanged;
+            // 
+            // results
+            // 
+            results.Location = new Point(101, 35);
+            results.Name = "results";
+            results.Size = new Size(421, 23);
+            results.TabIndex = 5;
+            results.TextChanged += results_TextChanged;
+            // 
+            // fileDate
+            // 
+            fileDate.Location = new Point(101, 93);
+            fileDate.Name = "fileDate";
+            fileDate.ReadOnly = true;
+            fileDate.Size = new Size(421, 23);
+            fileDate.TabIndex = 7;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(101, 64);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(421, 23);
+            dateTimePicker1.TabIndex = 8;
+            dateTimePicker1.ValueChanged += dateTimePicker1_ValueChanged;
+            // 
+            // folder
+            // 
+            folder.Location = new Point(12, 132);
+            folder.Name = "folder";
+            folder.Size = new Size(75, 23);
+            folder.TabIndex = 9;
+            folder.Text = "Folder";
+            folder.UseVisualStyleBackColor = true;
+            folder.Click += folder_Click;
+            // 
+            // save
+            // 
+            save.Enabled = false;
+            save.Location = new Point(93, 132);
+            save.Name = "save";
+            save.Size = new Size(75, 23);
+            save.TabIndex = 10;
+            save.Text = "Zapisz";
+            save.UseVisualStyleBackColor = true;
+            save.Click += save_Click;
+            // 
+            // open
+            // 
+            open.Enabled = false;
+            open.Location = new Point(174, 132);
+            open.Name = "open";
+            open.Size = new Size(75, 23);
+            open.TabIndex = 11;
+            open.Text = "Otwórz";
+            open.UseVisualStyleBackColor = true;
+            open.Click += open_Click;
+            // 
+            // randomExcuse
+            // 
+            randomExcuse.Enabled = false;
+            randomExcuse.Location = new Point(255, 132);
+            randomExcuse.Name = "randomExcuse";
+            randomExcuse.Size = new Size(267, 23);
+            randomExcuse.TabIndex = 12;
+            randomExcuse.Text = "Losowa wymówka";
+            randomExcuse.UseVisualStyleBackColor = true;
+            randomExcuse.Click += randomExcuse_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(538, 171);
+            Controls.Add(randomExcuse);
+            Controls.Add(open);
+            Controls.Add(save);
+            Controls.Add(folder);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(fileDate);
+            Controls.Add(results);
+            Controls.Add(description);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            Text = "Program do zarządzania wymówkami";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox description;
+        private TextBox results;
+        private TextBox fileDate;
+        private DateTimePicker dateTimePicker1;
+        private Button folder;
+        private Button save;
+        private Button open;
+        private Button randomExcuse;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private SaveFileDialog saveFileDialog1;
+        private OpenFileDialog openFileDialog1;
+    }
+}
---
+++ programy/rozdział9/Program 8 (Zarządzanie wymówkami - serjalizacja)/Program 2 (Zarządzanie wymówkami)/Form1.cs
@@ -0,0 +1,116 @@
+using System.Runtime.CompilerServices;
+
+namespace Program_2__Zarządzanie_wymówkami_
+{
+    public partial class Form1 : Form
+    {
+        private Excuse currentExcuse = new Excuse();
+        private string selectedFolder = "";
+        private bool formChanged = false;
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            currentExcuse.LastUsed = dateTimePicker1.Value;
+        }
+
+        private void folder_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = selectedFolder;
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                selectedFolder = folderBrowserDialog1.SelectedPath;
+                save.Enabled = true;
+                open.Enabled = true;
+                randomExcuse.Enabled = true;
+            }
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(description.Text) || String.IsNullOrEmpty(results.Text))
+            {
+                MessageBox.Show("Okreśł wymówkę i rezultat", "Nie można zapisać pliku", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            saveFileDialog1.InitialDirectory = selectedFolder;
+            saveFileDialog1.Filter = "Pliki wymówek (*.excuse)|*.excuse|Wszystkie pliki (*.*)|*.*";
+            saveFileDialog1.FileName = description.Text + ".excuse";
+            DialogResult result = saveFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                currentExcuse.Save(saveFileDialog1.FileName);
+                UpdateForm(false);
+                MessageBox.Show("Wymówka zapisana");
+            }
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            if (CheckChanged())
+            {
+                openFileDialog1.InitialDirectory = selectedFolder;
+                openFileDialog1.Filter = "Pliki wymówek (*.excuse)|*.excuse|Wszystkie pliki (*.*)|*.*|日本のファイル　（*.日本語）|*.日本語";
+                openFileDialog1.FileName = description.Text + ".excuse";
+                DialogResult result = openFileDialog1.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    currentExcuse = new Excuse(openFileDialog1.FileName);
+                    UpdateForm(false);
+                }
+            }
+        }
+
+        private void randomExcuse_Click(object sender, EventArgs e)
+        {
+            if (CheckChanged())
+            {
+                currentExcuse = new Excuse(random, selectedFolder);
+                UpdateForm(false);
+            }
+        }
+        private bool CheckChanged()
+        {
+            if (formChanged)
+            {
+                DialogResult result = MessageBox.Show("Bierząca wymówka nie została zapisana. Czy kontynuować?", "Ostrzeżenie", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (result == DialogResult.No) return false;
+            }
+            return true;
+        }
+
+        private void description_TextChanged(object sender, EventArgs e)
+        {
+            currentExcuse.Description = description.Text;
+            UpdateForm(true);
+        }
+
+        private void results_TextChanged(object sender, EventArgs e)
+        {
+            currentExcuse.Results = results.Text;
+            UpdateForm(true);
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            currentExcuse.LastUsed = dateTimePicker1.Value;
+            UpdateForm(true);
+        }
+        private void UpdateForm(bool changed)
+        {
+            if (!changed)
+            {
+                this.description.Text = currentExcuse.Description;
+                this.results.Text = currentExcuse.Results;
+                this.dateTimePicker1.Value = currentExcuse.LastUsed;
+                if (!String.IsNullOrEmpty(currentExcuse.ExcusePath))
+                    fileDate.Text = File.GetLastWriteTime(currentExcuse.ExcusePath).ToString();
+                this.Text = "Program do zarządzania wymówkami";
+            }
+            else this.Text = "Program do zarządzania wymówkami*";
+            this.formChanged = changed;
+        }
+    }
+
+}
