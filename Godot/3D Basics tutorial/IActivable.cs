1119d77352bbea4c75ef9bf2515153a395dd4c08
wojtexm2 <wblajda@gmail.com>
2023-10-02 13:11:49 +0200

    Part 5.1
    Drzwi i animacja otwierania/zamykania

---
+++ Godot/3D Basics tutorial/IActivable.cs
@@ -0,0 +1,5 @@
+public interface IActivable
+{
+    void Activate();
+    void Decativate();
+}
---
+++ Godot/3D Basics tutorial/door.cs
@@ -0,0 +1,36 @@
+using Godot;
+using System;
+
+public partial class door : StaticBody3D, IActivable
+{
+	[Export] NodePath AnimationPlayerNodePath;
+	AnimationPlayer AnimationPlayer;
+	private bool isOpen = false;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		AnimationPlayer = GetNode<AnimationPlayer>(AnimationPlayerNodePath);
+		AnimationPlayer.AssignedAnimation = AnimationPlayer.GetAnimationList()[0];
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public void Activate()
+	{
+		if (isOpen) return;
+		isOpen = true;
+		AnimationPlayer.Play();
+	}
+	public void Decativate()
+	{
+		AnimationPlayer.PlayBackwards();
+		isOpen = false;
+	}
+	public void Activate(Node caller)
+	{
+		Activate();
+	}
+}
