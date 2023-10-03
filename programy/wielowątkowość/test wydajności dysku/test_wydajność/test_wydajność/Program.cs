7239622748ea04917d5339f8fcce1f95d93b1564
wojtexm2 <wblajda@gmail.com>
2023-10-03 13:49:26 +0200

    User prompt and test multi threading

---
+++ programy/wielowątkowość/test wydajności dysku/test_wydajność/test_wydajność/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace test_wydajność
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path;
+            int threadCount;
+            int size;
+            Thread[] Threads;
+
+            path = UserPrompt.PromptForPath();
+            threadCount = UserPrompt.PromptForThreads();
+            size = UserPrompt.PromptForSize();
+            Threads = new Thread[threadCount];
+
+            for (int i = 0; i < threadCount; i++)
+            {
+                Threads[i] = new Thread(fnct);
+                Threads[i].Start();
+            }
+            for (int i = 0; i < threadCount; i++)
+            {
+                Threads[i].Join();
+            }
+
+            
+        }
+        public static void fnct()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("I exist!");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ programy/wielowątkowość/test wydajności dysku/test_wydajność/test_wydajność/UserPrompt.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_wydajność
+{
+    internal class UserPrompt
+    {
+        public static string PromptForPath()
+        {
+            string path;
+            Console.WriteLine("Wybierz ścieżkę do folderu");
+            while (true)
+            {
+                path = Console.ReadLine();
+                if (!Directory.Exists(path)) Console.WriteLine("Podaj właściwą ścieżkę do folderu!");
+                else break;
+            }
+            return path;
+        }
+        public static int PromptForThreads()
+        {
+            int threads;
+
+            Console.WriteLine("Podaj ilość wątków");
+            while (true)
+            {
+                string count = Console.ReadLine();
+                if (!int.TryParse(count, out threads)) Console.WriteLine("Podaj właściwą liczbę!");
+                else if (threads < 1) Console.WriteLine("Ilość wątków musi być większa od 0!");
+                else break;
+            }
+
+            return threads;
+        }
+        public static int PromptForSize()
+        {
+            int size;
+
+            Console.WriteLine("Podaj rozmiar danych testowych w megabajtach");
+            while (true)
+            {
+                string sizeString = Console.ReadLine();
+                if (!int.TryParse(sizeString, out size)) Console.WriteLine("Podaj właściwą liczbę!");
+                else if (size < 1) Console.WriteLine("Ilość danych musi być większa od 0!");
+                else break;
+            }
+            return size;
+        }
+    }
+}
